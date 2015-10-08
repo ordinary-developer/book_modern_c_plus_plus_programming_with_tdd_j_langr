7876185ebb0ebe2d5142aa1bb568bb051ce45eaf
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-10-08 18:36:33 +0300

    add the first test
    add the first test to the project

---
+++ code/chapter_2_TDD_A_FIRST_EXAMPLE/CMakeFiles/test_application.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_application.dir/main.cpp.o"
+  "CMakeFiles/test_application.dir/SoundexTest.cpp.o"
+  "test_application.pdb"
+  "test_application"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_application.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
