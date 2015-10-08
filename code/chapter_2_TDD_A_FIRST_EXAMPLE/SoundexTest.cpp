b98edaf648d591de179856f1270afcefc22561d6
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-10-08 18:54:03 +0300

    add the padding behavior for algorithm
    add the padding with zeros behavior for
    the current algorithm

--- code/chapter_2_TDD_A_FIRST_EXAMPLE/SoundexTest.cpp
+++ code/chapter_2_TDD_A_FIRST_EXAMPLE/SoundexTest.cpp
@@ -3,7 +3,11 @@
 class Soundex {
     public:
         std::string encode(const std::string& word) const {
-            return word;
+            return zeroPad(word);
+        }
+    private:
+        std::string zeroPad(const std::string& word) const {
+            return word + "000";
         }
 };
 
@@ -15,6 +19,13 @@ TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
 
     auto encoded = soundex.encode("A");
 
-    ASSERT_THAT(encoded, testing::Eq("A"));
+    ASSERT_THAT(encoded, testing::Eq("A000"));
 }
 
+TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
+    Soundex soundex;
+
+    auto encoded = soundex.encode("I");
+
+    ASSERT_THAT(encoded, testing::Eq("I000"));
+}
