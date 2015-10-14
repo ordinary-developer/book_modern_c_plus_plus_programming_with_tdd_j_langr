49c778ca63db043d541645a37101f5deb8ff4909
ordinary-developer <merely.ordinary.developer@gmail.com>
2015-10-14 16:55:48 +0300

    2.13 - add the "CombinesDuplicateEncodings" test
    add the "CombinesDuplicateEncodings" test
    and production code for this test

--- code/chapter_2_TDD_A_FIRST_EXAMPLE/Soundex.hpp
+++ code/chapter_2_TDD_A_FIRST_EXAMPLE/Soundex.hpp
@@ -12,6 +12,20 @@ class Soundex {
         std::string encode(const std::string& word) const {
             return zeroPad(head(word) + encodedDigits(tail(word)));
         }
+
+        std::string encodedDigit(char letter) const {
+            const std::unordered_map<char, std::string> encodings {
+                {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+                {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
+                            {'s', "2"}, {'x', "2"}, {'z', "2"},
+                {'d', "3"}, {'t', "3"},
+                {'l', "4"}, 
+                {'m', "5"}, {'n', "5"},
+                {'r', "6"}
+            };
+            auto iterator = encodings.find(letter);
+            return end(encodings) == iterator ? "" : iterator->second;
+        }
     private:
         std::string head(const std::string& word) const {
             return word.substr(0, 1);
@@ -27,27 +41,22 @@ class Soundex {
                 if (isComplete(encoding)) {
                     break;
                 }
-                encoding += encodedDigit(letter);
+                if (encodedDigit(letter) != lastDigit(encoding)) {
+                    encoding += encodedDigit(letter);
+                }
             }
             return encoding;
         }
 
-        bool isComplete(const std::string& encoding) const {
-            return encoding.length() == MaxCodeLength - 1;
+        std::string lastDigit(const std::string& encoding) const {
+            if (encoding.empty()) {
+                return "";
+            }
+            return std::string(1, encoding.back());
         }
 
-        std::string encodedDigit(char letter) const {
-            const std::unordered_map<char, std::string> encodings {
-                {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
-                {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"},
-                            {'s', "2"}, {'x', "2"}, {'z', "2"},
-                {'d', "3"}, {'t', "3"},
-                {'l', "4"}, 
-                {'m', "5"}, {'n', "5"},
-                {'r', "6"}
-            };
-            auto iterator = encodings.find(letter);
-            return end(encodings) == iterator ? "" : iterator->second;
+        bool isComplete(const std::string& encoding) const {
+            return encoding.length() == MaxCodeLength - 1;
         }
 
         std::string zeroPad(const std::string& word) const {
