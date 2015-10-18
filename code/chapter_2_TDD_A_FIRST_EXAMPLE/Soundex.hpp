2adfe7dbf0cc12f5864ffa827854e615de7c0b4a
ivan <ivan@sample_mail_server.ru>
2015-10-18 15:59:09 +0300

    2.16 - make some refactoring
    make some refactoring
    
    after the commit
    - some code has been refactored
    - all tests pass

--- code/chapter_2_TDD_A_FIRST_EXAMPLE/Soundex.hpp
+++ code/chapter_2_TDD_A_FIRST_EXAMPLE/Soundex.hpp
@@ -43,20 +43,29 @@ class Soundex {
         }
 
         std::string encodedDigits(const std::string& word) const {
-            std::string encoding {""};
+            std::string encoding;
+            encodeHead(encoding, word);
+            encodeTail(encoding, word);
+            return encoding;
+        }
 
+        void encodeHead(std::string& encoding, const std::string& word) const {
             encoding += encodedDigit(word.front());
+        }
 
-            for (auto letter : tail(word)) {
-                if (isComplete(encoding)) {
-                    break;
-                }
-                auto digit = encodedDigit(letter);
-                if (digit != NotADigit && digit != lastDigit(encoding)) {
-                    encoding += digit;
+        void encodeTail(std::string& encoding, const std::string& word) const {
+            for (auto letter: tail(word)) {
+                if (!isComplete(encoding)) {
+                    encodeLetter(encoding, letter);
                 }
             }
-            return encoding;
+        }
+
+        void encodeLetter(std::string& encoding, char letter) const {
+            auto digit = encodedDigit(letter);
+            if (digit != NotADigit && digit != lastDigit(encoding)) {
+                encoding += digit;
+            }
         }
 
         std::string lastDigit(const std::string& encoding) const {
